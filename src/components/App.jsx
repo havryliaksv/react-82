2fd3f89c1c3ca453fd238fe406447d360e8fa461
havryliaksv <gavriliaks@gmail.com>
2024-03-28 14:06:09 +0200

    edit work

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,9 @@ import PaymentList from "./PaymentList";
 import PageTitle from "./PageTitle";
 import initialPayments from "../payments.json";
 export default function App() {
-  //  console.log(payments);
   return (
     <>
-      <PageTitle text="Payments" />
+      <PageTitle text="List of Payments" />
       <PageTitle text="Profile" />
       <PaymentList payments={initialPayments} />
 
